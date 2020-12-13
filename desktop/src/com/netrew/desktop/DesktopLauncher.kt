b897a02a1826ca388aadf1036287a1c7b3bfc426
vladislavnikolaev <yavl>
2020-12-14 02:59:32 +0900

    lwjgl3 to lwjgl2

--- desktop/src/com/netrew/desktop/DesktopLauncher.kt
+++ desktop/src/com/netrew/desktop/DesktopLauncher.kt
@@ -1,15 +1,16 @@
 package com.netrew.desktop
 
-import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
-import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.netrew.Main
 
 object DesktopLauncher {
     @JvmStatic
     fun main(arg: Array<String>) {
-        val config = Lwjgl3ApplicationConfiguration()
-        config.setTitle("Netrew")
-        config.setWindowedMode(1600, 1200)
-        Lwjgl3Application(Main(), config)
+        val config = LwjglApplicationConfiguration()
+        config.title = "Netrew"
+        config.width = 800
+        config.height = 600
+        LwjglApplication(Main(), config)
     }
 }
